cd3d3249ed7ca1b3f732a82ab338c911810a2340
Daniel <bazorov.dany@gmail.com>
2025-11-06 13:37:27 +0300

    added section "skills" with script

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,18 @@
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
+      closeElem = document.querySelector('.menu__close'),
+      counters = document.querySelectorAll('.skills__ratings-counter'),
+      lines = document.querySelectorAll('.skills__ratings-line span');
+
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
-})
+});
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
-})
+});
 
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
